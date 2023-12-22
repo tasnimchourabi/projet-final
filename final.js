241a97b24557a7310e2191bb85edc0212948fef8
tasnim <tasnimchourabi2002@gmail.com>
2023-12-22 23:22:09 +0100

    first commit

--- final.js
+++ final.js
@@ -11,3 +11,13 @@ function animatePage(href) {
     setTimeout(function() {
         window.location.href = href;
     }, 500); }
+    function displayMessage(cardType) {
+        if (cardType === 'about') {
+            alert("About Titans Gym: Titans Gym is a premier fitness center offering state-of-the-art facilities and personalized training programs.");
+        } else if (cardType === 'history') {
+            alert("History: Established in 2005, Titans Gym has been a pioneer in the fitness industry, catering to fitness enthusiasts of all levels.");
+        } else if (cardType === 'popularity') {
+            alert("Popularity: Titans Gym has gained immense popularity for its top-notch equipment, expert trainers, and diverse workout programs.");
+        }
+    }
+ 
